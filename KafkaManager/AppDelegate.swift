cd1b812efd78a49e5efb2066180797833e8b4faa
Kellan Cummings <kellan.burket@gmail.com>
2016-01-08 13:47:56 -0800

    Initial Commit

---
+++ KafkaManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KafkaManager
+//
+//  Created by Kellan Cummings on 1/8/16.
+//  Copyright © 2016 Kellan Cummings. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KafkaManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KafkaManager
+//
+//  Created by Kellan Cummings on 1/8/16.
+//  Copyright © 2016 Kellan Cummings. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
